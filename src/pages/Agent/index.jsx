2614f5c9c4fc6087959dfc6bf63c44538d5b1b20
keith <mwanazk@gmail.com>
2025-06-16 13:52:31 +0100

    barrel

---
+++ src/pages/Agent/index.jsx
@@ -0,0 +1,9 @@
+export { default as Dashboard } from "./Dashboard/Dashboard.jsx";
+export { default as VehicleList } from "./fleet/VehicleList.jsx";
+export { default as AddVehicle } from "./fleet/AddVehicle.jsx";
+export { default as EditVehicle } from "./fleet/EditVehicle.jsx";
+export { default as MaintenanceRecords } from "./fleet/MaintenanceRecords.jsx";
+export { default as Unavailability } from "./fleet/Unavailability.jsx";
+
+
+// and so on...
